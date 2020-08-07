c841e5ef7a97e0e1ed7e5cf50511dccf04d6d739
richardrugg <richard@ruggcomputers.com>
2020-08-07 01:40:05 -0500

    Final login script.

--- login.py
+++ login.py
@@ -1,7 +1,5 @@
 import dbm  # database library
-"""
-import getpass  # console input hiding, may come back to this later
-"""
+#  import getpass  # console input hiding, may come back to this later
 
 
 def check_if_blank(x):
@@ -23,36 +21,52 @@ def password_input_confirm(p1, p2):
 def authenticate(u, p):
     #  returns True if entered credentials match stored credentials
     with dbm.open('credentials', 'r') as c:
-        if c[u].decode("utf-8") == p:
+        u2 = c.get(u)
+        #  u2 created to get value of username as key
+        if u2 is None:
+            #  checks if u2 is None (doesn't exist in database)
+            return False
+        elif c[u].decode("utf-8") == p:
             #  decodes bytes retrieved from database value
-            c.close()
             return True
         else:
             return False
 
 
-action_type = input('Select Option: \n 1. Login\n 2. Create Account\n')
-if action_type == '1':
-    username = input('Username: ')
-    password = input('Password: ')
-    with dbm.open('credentials', 'r') as cred:
-        #  open database in readonly mode
-        if authenticate(username, password) is True:
-            print('Authenticated')
+def login(u, p):
+    with dbm.open('credentials', 'r') as c:
+        if authenticate(u, p) is True:
+            print('Successfully Authenticated!\n')
+            c.close()
         else:
-            print('Failed to Authenticate')
-elif action_type == '2':
-    username = input('Username: ')
-    password = input('Password: ')
-    confirm_password = input('Password: ')
-    if password_input_confirm(password, confirm_password) is True:
-        with dbm.open('credentials', 'c') as cred:
-            #  open database in mode to create and update entries
-            cred[username] = password
-            cred.close()
+            print('Invalid Username / Password.\n')
+            c.close()
+            login_process()
+
+
+def login_process():
+    global action_type
+    action_type = input('Select Option: \n 1. Login\n 2. Create Account\n')
+    if action_type == '1':
+        username = input('Username: ')
+        password = input('Password: ')
+        login(username, password)
+    elif action_type == '2':
+        username = input('Username: ')
+        password = input('Password: ')
+        confirm_password = input('Password: ')
+        if password_input_confirm(password, confirm_password) is True:
+            with dbm.open('credentials', 'c') as cred:
+                #  open database in mode to create and update entries
+                cred[username] = password
+                print('Account Created!\n')
+                login_process()
+        else:
+            print('Passwords do not match, or are blank.\n')
+            login_process()
     else:
-        print('Passwords do not match, or are blank.')
-        #  TODO find way to return mismatched input passwords back to initial prompt.
-else:
-    print('Invalid option entered.')
-    #  TODO find a way to return invalid option input back to initial prompt.
+        print('Invalid option entered.\n')
+        login_process()
+
+
+login_process()
